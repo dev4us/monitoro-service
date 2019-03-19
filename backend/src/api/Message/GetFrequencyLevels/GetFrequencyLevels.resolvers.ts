c8abe749ef398eba5506e7ef8495da5e2986a531
dev4us <bronxofsoul@gmail.com>
2019-03-19 23:07:32 +0900

    [BE] fix to GetFrequencyLevels

--- backend/src/api/Message/GetFrequencyLevels/GetFrequencyLevels.resolvers.ts
+++ backend/src/api/Message/GetFrequencyLevels/GetFrequencyLevels.resolvers.ts
@@ -1,59 +1,62 @@
-import { Resolvers } from "src/types/resolvers";
-import privateResolver from "src/utils/privateResolvers";
-import { GetFrequencyLevelsResponse, GetFrequencyLevelsqueryArgs } from "src/types/graphql";
-import Message from "src/entities/Message";
-import { getRepository } from "typeorm";
-import Project from "src/entities/Project";
+import { Resolvers } from "../../../../src/types/resolvers";
+import privateResolver from "../../../../src/utils/privateResolvers";
+import {
+  GetFrequencyLevelsResponse,
+  GetFrequencyLevelsqueryArgs
+} from "../../../../src/types/graphql";
+import Message from "../../../../src/entities/Message";
+import Project from "../../../../src/entities/Project";
 
-const resolvers:Resolvers = {
-  Query:{
-    GetFrequencyLevels:privateResolver(
-      async (_, args:GetFrequencyLevelsqueryArgs, {req}):Promise<GetFrequencyLevelsResponse> => {
-        const {user} = req;
-        const {projectId} = args;
+const resolvers: Resolvers = {
+  query: {
+    GetFrequencyLevels: privateResolver(
+      async (
+        _,
+        args: GetFrequencyLevelsqueryArgs,
+        { req }
+      ): Promise<GetFrequencyLevelsResponse> => {
+        const { user } = req;
+        const { projectId } = args;
 
-        try{
-          const accessAble = await Project.findOne({participants: user, id:projectId});
+        try {
+          const accessAble = await Project.findOne({
+            participants: user,
+            id: projectId
+          });
 
-          if(!accessAble){
-            return{
-              ok:false,
+          if (!accessAble) {
+            return {
+              ok: false,
               error: "Not found Project or You Don't have Auth",
-              frequency:null
-            }
+              frequency: null
+            };
           }
 
-          const notice = await Message.count({projectId, level: "NOTICE"});
-          const debug = await Message.count({projectId, level: "DEBUG"});
-          const warning = await Message.count({projectId, level: "WARNING"});
-          const danger = await Message.count({projectId, level: "DANGER"});
+          const notice = await Message.count({ projectId, level: "NOTICE" });
+          const debug = await Message.count({ projectId, level: "DEBUG" });
+          const warning = await Message.count({ projectId, level: "WARNING" });
+          const danger = await Message.count({ projectId, level: "DANGER" });
 
-          return{
-            ok:true,
-            error:null,
+          return {
+            ok: true,
+            error: null,
             frequency: {
               notice,
               debug,
               warning,
               danger
             }
-          }
-        }catch(error){
-          return{
+          };
+        } catch (error) {
+          return {
             ok: false,
             error: error.message,
-            frequency:null
-          }
+            frequency: null
+          };
         }
-        
-        
-        const NoticeCount = await getRepository(Message).createQueryBuilder('message').
-        const DebugCount = 
-        const WarningCount = 
-        const DangerCount = 
       }
     )
   }
-}
+};
 
 export default resolvers;
