578aec6cb0283d3a5bc3dfff53fa5c7505cee061
dev4us <bronxofsoul@gmail.com>
2019-03-19 23:03:44 +0900

    [BE] GetFrequencyLevels

--- backend/src/types/graphql.d.ts
+++ backend/src/types/graphql.d.ts
@@ -1,4 +1,4 @@
-export const typeDefs = ["type GetMessageResponse {\n  ok: Boolean!\n  error: String\n  message: Message\n  msgCount: Int!\n  similarMsg: [Message]\n}\n\ntype Query {\n  GetMessage(projectId: Int!, messageId: Int!): GetMessageResponse!\n  GetMessages(projectId: Int!): GetMessagesResponse!\n  GetProject(projectId: Int!): GetProjectResponse!\n  GetProjects: GetProjectsResponse!\n  GetTags(projectId: Int!): GetTagsResponse!\n  GetProfile: GetProfileResponse!\n  User: String!\n}\n\ntype GetMessagesResponse {\n  ok: Boolean!\n  error: String\n  messages: [Message]\n}\n\ntype SendMessageResponse {\n  ok: Boolean!\n  error: String\n}\n\ninput attachTags {\n  name: String!\n  color: String\n}\n\nenum levelOption {\n  NOTICE\n  WARNING\n  DEBUG\n  DANGER\n}\n\ntype Mutation {\n  SendMessage(level: levelOption!, contents: String!, location: String, apiKey: String!, tags: [attachTags]): SendMessageResponse!\n  CreateProject(projectName: String!, description: String, thumbnail: String): CreateProjectResponse!\n  CreateTag(projectId: Int!, name: String!, color: String): CreateTagResponse!\n  SignIn(userEmail: String!, userName: String!, profileImage: String): SignInResponse!\n}\n\ntype Subscription {\n  SendMessageSubscription: Message\n  CreateTagSubscription: Tag\n}\n\ntype Message {\n  id: Int!\n  level: String\n  contents: String!\n  location: String\n  project: Project\n  projectId: Int\n  tags: [Tag]\n  createdAt: String!\n}\n\ntype CreateProjectResponse {\n  ok: Boolean!\n  error: String\n  project: Project\n}\n\ntype GetProjectResponse {\n  ok: Boolean!\n  error: String\n  project: Project\n}\n\ntype GetProjectsResponse {\n  ok: Boolean!\n  error: String\n  projects: [Project]\n}\n\ntype Project {\n  id: Int!\n  name: String!\n  thumbnail: String\n  description: String\n  participants: [User]!\n  admin: User\n  messages: [Message]\n  tags: [Tag]\n  apiKey: String!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype CreateTagResponse {\n  ok: Boolean!\n  error: String\n  tag: Tag\n}\n\ntype GetTagsResponse {\n  ok: Boolean!\n  error: String\n  tags: [Tag]\n}\n\ntype Tag {\n  id: Int!\n  name: String!\n  color: String\n  project: Project!\n  projectId: Int\n  createdAt: String!\n  updatedAt: String\n}\n\ntype GetProfileResponse {\n  ok: Boolean!\n  error: String\n  user: User\n}\n\ntype User {\n  id: Int!\n  userName: String!\n  userEmail: String!\n  profileImage: String\n  innerProjects: [Project]\n  manages: [Project]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignInResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n"];
+export const typeDefs = ["type GetFrequencyLevelsResponse {\n  ok: Boolean!\n  error: String\n  frequency: LevelOption\n}\n\ntype LevelOption {\n  notice: Int!\n  debug: Int!\n  warning: Int!\n  danger: Int!\n}\n\ntype query {\n  GetFrequencyLevels(projectId: Int!): GetFrequencyLevelsResponse!\n}\n\ntype GetMessageResponse {\n  ok: Boolean!\n  error: String\n  message: Message\n  msgCount: Int!\n  similarMsg: [Message]\n}\n\ntype Query {\n  GetMessage(projectId: Int!, messageId: Int!): GetMessageResponse!\n  GetMessages(projectId: Int!): GetMessagesResponse!\n  GetProject(projectId: Int!): GetProjectResponse!\n  GetProjects: GetProjectsResponse!\n  GetTags(projectId: Int!): GetTagsResponse!\n  GetProfile: GetProfileResponse!\n  User: String!\n}\n\ntype GetMessagesResponse {\n  ok: Boolean!\n  error: String\n  messages: [Message]\n}\n\ntype SendMessageResponse {\n  ok: Boolean!\n  error: String\n}\n\ninput attachTags {\n  name: String!\n  color: String\n}\n\nenum levelOption {\n  NOTICE\n  WARNING\n  DEBUG\n  DANGER\n}\n\ntype Mutation {\n  SendMessage(level: levelOption!, contents: String!, location: String, apiKey: String!, tags: [attachTags]): SendMessageResponse!\n  CreateProject(projectName: String!, description: String, thumbnail: String): CreateProjectResponse!\n  CreateTag(projectId: Int!, name: String!, color: String): CreateTagResponse!\n  SignIn(userEmail: String!, userName: String!, profileImage: String): SignInResponse!\n}\n\ntype Subscription {\n  SendMessageSubscription: Message\n  CreateTagSubscription: Tag\n}\n\ntype Message {\n  id: Int!\n  level: String\n  contents: String!\n  location: String\n  project: Project\n  projectId: Int\n  tags: [Tag]\n  createdAt: String!\n}\n\ntype CreateProjectResponse {\n  ok: Boolean!\n  error: String\n  project: Project\n}\n\ntype GetProjectResponse {\n  ok: Boolean!\n  error: String\n  project: Project\n}\n\ntype GetProjectsResponse {\n  ok: Boolean!\n  error: String\n  projects: [Project]\n}\n\ntype Project {\n  id: Int!\n  name: String!\n  thumbnail: String\n  description: String\n  participants: [User]!\n  admin: User\n  messages: [Message]\n  tags: [Tag]\n  apiKey: String!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype CreateTagResponse {\n  ok: Boolean!\n  error: String\n  tag: Tag\n}\n\ntype GetTagsResponse {\n  ok: Boolean!\n  error: String\n  tags: [Tag]\n}\n\ntype Tag {\n  id: Int!\n  name: String!\n  color: String\n  project: Project!\n  projectId: Int\n  createdAt: String!\n  updatedAt: String\n}\n\ntype GetProfileResponse {\n  ok: Boolean!\n  error: String\n  user: User\n}\n\ntype User {\n  id: Int!\n  userName: String!\n  userEmail: String!\n  profileImage: String\n  innerProjects: [Project]\n  manages: [Project]\n  createdAt: String!\n  updatedAt: String\n}\n\ntype SignInResponse {\n  ok: Boolean!\n  error: String\n  token: String\n}\n"];
 /* tslint:disable */
 
 export interface Query {
@@ -178,4 +178,25 @@ export interface SignInResponse {
 export interface Subscription {
   SendMessageSubscription: Message | null;
   CreateTagSubscription: Tag | null;
+}
+
+export interface GetFrequencyLevelsResponse {
+  ok: boolean;
+  error: string | null;
+  frequency: LevelOption | null;
+}
+
+export interface LevelOption {
+  notice: number;
+  debug: number;
+  warning: number;
+  danger: number;
+}
+
+export interface query {
+  GetFrequencyLevels: GetFrequencyLevelsResponse;
+}
+
+export interface GetFrequencyLevelsqueryArgs {
+  projectId: number;
 }
